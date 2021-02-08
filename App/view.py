8aeb12d44c82ee97b7a572e219b9129d3bc00cc7
NicoleMurillo <n.murillof@uniandes.edu.co>
2021-02-08 18:47:15 -0500

    Entrega Final – laboratorio 2

--- App/view.py
+++ App/view.py
@@ -73,6 +73,7 @@ Menu principal
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
+    
     if int(inputs[0]) == 1:
         print("Cargando información de libros....")
         books = loadBooks()
